4c41a958d0d99029975626923d4289b748cfffcd
Ashok Bhaargaw <ashokbhaargaw@gmail.com>
2026-02-11 01:41:29 +0530

    improved cahrt

--- app/admin/(dashboard)/Charts.tsx
+++ app/admin/(dashboard)/Charts.tsx
@@ -24,23 +24,31 @@ export default function Charts({ data }: any) {
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-12 gap-4 sm:gap-6">
-
       {/* Events Over Time */}
       <div className="lg:col-span-8">
         <Card title="Events Over Time">
-          <ResponsiveContainer width="100%" height={220}>
-            <LineChart
-              data={data.daily.map((d: any) => ({
-                date: d._id.slice(5), // MM-DD for mobile
-                count: d.count,
-              }))}
+          <div className="overflow-x-auto flex items-center justify-end -ml-2 -mr-2 ">
+            <div
+              className="min-w-105 px-2 "
+              style={{
+                minWidth: Math.max(420, data.daily.length * 28),
+              }}
             >
-              <XAxis dataKey="date" tick={{ fontSize: 10 }} />
-              <YAxis hide />
-              <Tooltip />
-              <Line dataKey="count" strokeWidth={2} dot />
-            </LineChart>
-          </ResponsiveContainer>
+              <ResponsiveContainer width="100%" height={220}>
+                <LineChart
+                  data={data.daily.map((d: any) => ({
+                    date: d._id.slice(5), // MM-DD for mobile
+                    count: d.count,
+                  }))}
+                >
+                  <XAxis dataKey="date" tick={{ fontSize: 10 }} />
+                  <YAxis hide />
+                  <Tooltip />
+                  <Line dataKey="count" strokeWidth={2} dot />
+                </LineChart>
+              </ResponsiveContainer>
+            </div>
+          </div>
         </Card>
       </div>
 
@@ -87,7 +95,6 @@ export default function Charts({ data }: any) {
           </div>
         </Card>
       </div>
-
     </div>
   );
 }
